4357207cf3768b139fbecf6d60447db03cd98ffc
sltries <sltries@umich.edu>
2020-04-28 19:15:59 -0400

    Initial commit

---
+++ fueltracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fueltracker
+//
+//  Created by Stevie Triesenberg on 4/28/20.
+//  Copyright © 2020 Bayflight. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
